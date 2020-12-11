217b7004182c367ba8c838e62c7a8fc219265913
Lukas Nowy <lukas.n912@gmail.com>
2020-12-11 23:58:44 +0100

    Created Projects

---
+++ SWE1-MCTG/SWE1-MCTG-TEST/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SWE1_MCTG_TEST
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
